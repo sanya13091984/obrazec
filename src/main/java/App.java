e6d76f6b12b4335d0052d077e5ab3b66d52117c2
sanya13091984 <flaaa965@gmail.com>
2020-03-11 15:11:26 +0300

    sex

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("volk");
         System.out.println("tigr lev");
-
+        System.out.println("sex viski");
 
     }
 }
