54b4e7c9db7db1f3ed893b70e60fca347d17d95d
sanya13091984 <flaaa965@gmail.com>
2020-03-11 13:49:16 +0300

    moskwa

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("skoroo");
         System.out.println("letoo");
+        System.out.println("priven mockow");
 
 
     }
