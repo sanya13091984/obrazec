79a5db73ab6b6cf66bf0d8e3807de42d037854f1
sanya13091984 <flaaa965@gmail.com>
2020-03-11 15:19:08 +0300

    2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,13 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("volk");
-        System.out.println("tigr lev");
+        System.out.println("leto");
+        System.out.println("zima              ");
         System.out.println("sex viski");
+
+
+        //
+
 
     }
 }
