0c46397dbbf559f48172ac3c15a1030643981c99
sanya13091984 <flaaa965@gmail.com>
2020-03-11 15:06:03 +0300

    zver

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,8 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("leto");
-        System.out.println("zima              ");
+        System.out.println("volk");
+        System.out.println("tigr");
 
 
     }
