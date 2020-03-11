dc8bb7e42ebe6a332e520d84e8929ba8d24b32c2
sanya13091984 <61901299+sanya13091984@users.noreply.github.com>
2020-03-11 14:56:53 +0300

    probeli

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("leto");
-        System.out.println("zima");
+        System.out.println("zima              ");
 
     }
 }
