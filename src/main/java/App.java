643fbcc61a3d8ee89bca2504748e281f28dca158
sanya13091984 <flaaa965@gmail.com>
2020-03-11 14:09:52 +0300

    udalil 2 stroki

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("leto");
-        System.out.println("suka"); // commit push master
-        System.out.println("priven mockow");
+        System.out.println("zima");
+
     }
 }
