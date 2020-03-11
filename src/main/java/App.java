4ce5b303928327f4928cf30763ad4ab40acd306a
sanya13091984 <flaaa965@gmail.com>
2020-03-11 12:17:49 +0300

    leto

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,9 +2,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("lol");
-        System.out.println("prosti");
-        System.out.println("lew");
+        System.out.println("suka");
+
+
     }
-
-
 }
