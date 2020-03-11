3ab521a90845955c91c5e69e5ef0aab81d5d036b
sanya13091984 <flaaa965@gmail.com>
2020-03-11 15:34:28 +0300

    udalili

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,15 @@
 public class App {
 
     public static void main(String[] args) {
+        System.out.println("leto");
+        //
+        System.out.println("ДОБРЫЙ ДЕНЬ МИР!!!");           //пока
+
+        System.out.println("roma");
+
+        System.out.println('ю');
+                                                    //привет!!!
 
 
-        System.out.println("sex viski");
-
-
-   }
+    }
 }
