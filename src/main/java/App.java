a3b0c07bb09ed0a63161c023d0ea41e03d383c2b
sanya13091984 <flaaa965@gmail.com>
2020-03-11 15:35:41 +0300

    com mas

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,10 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("leto");
-        System.out.println("zima              ");
+
+
         System.out.println("sex viski");
 
 
-        //
-
-
-    }
+   }
 }
