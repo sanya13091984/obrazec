e5d3e0def620fb1d3045dff1c5dee40df6126c2c
sanya13091984 <61901299+sanya13091984@users.noreply.github.com>
2020-03-11 14:10:31 +0300

    udalil 4 stroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,6 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("skoro");
         System.out.println("leto");
         System.out.println("suka"); // commit push master
         System.out.println("priven mockow");
